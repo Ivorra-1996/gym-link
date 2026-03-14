6c125c7412fcf7c60ba0e0360c5d24564d7397c5
Ivorra-1996 <ivorrajosemaria@gmail.com>
2026-03-14 02:09:29 -0300

    fix

--- src/constants/tailwind-runtime-theme.ts
+++ src/constants/tailwind-runtime-theme.ts
@@ -2,3 +2,10 @@ const { tailwindRuntimeTheme } = require("../../tailwind.config.tsx");
 
 export const twColors = tailwindRuntimeTheme.colors;
 export const twRadius = tailwindRuntimeTheme.borderRadius;
+export const borderWidth = tailwindRuntimeTheme.borderWidth;
+
+export const twFonts = {
+  regular: "SpaceGrotesk_400Regular",
+  medium: "SpaceGrotesk_500Medium",
+  bold: "SpaceGrotesk_700Bold",
+};
